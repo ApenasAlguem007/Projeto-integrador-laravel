4998bf55b4f0c666f7d3fd9dd2dd10d253a560b4
ApemasAlguem007 <geovanna.psilva2005@gmail>
2025-06-11 12:04:25 -0300

    projeto-intgrador-laravel

--- routes/web.php
+++ routes/web.php
@@ -1,48 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/sobrenos', function (){
-    return 'Essa é a pagina sobre nós';
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get ('/home', function (){
-    return 'Essa é a página de home';
-});
-
-Route::get ('/cadastro', function (){
-    return 'Essa é a página de cadastro';
-});
-
-Route::get ('/login', function () {
-    return 'Essa é a página de login';
-});
-
-Route::get ('/recuperacao', function (){
-    return 'Essa é a página de recuperação de senha';
-});
-
-Route::get ('/curso', function (){
-    return 'Essa é a página de curso';
-});
-
-Route::get ('/detalhes', function (){
-    return 'Essa é a página de detalhes do curso';
-});
-
-Route::get ('/meuscursos', function (){
-    return 'Essa é a página dos meus cursos';
-});
-
-Route::get ('/perfil', function (){
-    return 'Essa é a página de perfil';
-});
-
-Route::get ('/licoes', function (){
-    return 'Essa é a página de lições';
-});
-
+require __DIR__.'/auth.php';
