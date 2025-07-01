57ff73903ddbe34026048f25070b9adf39c68036
ApenasAlguem007 <geovanna.psilva2005@gmail.com>
2025-07-01 11:49:45 -0300

    adicionando telas e styles

---
+++ resources/views/courses.blade.php
@@ -0,0 +1,132 @@
+@include('components/include.header')
+
+<div class="titulo-courses">
+ <h1>HARDWARE</h1>
+</div>
+    <div class="hard">
+ 
+    <div class="modelo">
+    <h2>Front-End</h2>
+    </div>
+ 
+ <div id="carouselExampleControls" class="carousel slide">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="{{ asset('images/conceito-de-engenharia-de-computador-plana.png') }}" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="{{ asset('images/Wallpaper – Back-End II 1.png') }}" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="{{ asset('images/Wallpaper – Back-End I 1.png') }}" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+ 
+<br>
+ 
+<div class="detalhes">
+<a class="detalhes-botao"href="{{ asset('../desc') }}"><button class="detalhes-btn">Detalhe</button></a>
+</div>
+</div>
+ 
+ 
+ 
+<br><br>
+<br><br>
+<br><br>
+<div class="titulo-courses">
+ 
+<h1>NETWORK</h1>
+</div>
+<div class="network">
+<br>
+    <div class="modelo">
+        <h2>Redes</h2>
+    </div>
+       
+        <div id="carouselExampleControls" class="carousel slide">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="{{ asset('images/Wallpaper – Back-End I 1.png') }}" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="{{ asset('images/Wallpaper – Back-End II 1.png') }}" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="{{ asset('images/Wallpaper – Back-End I 1.png') }}" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+ 
+<br>
+<div class="detalhes">
+<a class="detalhes-botao"href="{{ asset('../desc') }}"><button class="detalhes-btn">Detalhe</button></a>
+</div>
+</div>
+ 
+<br><br>
+<br><br>
+<br><br>
+<div class="titulo-courses">
+<h1>SOFTWARE</h1>
+</div>
+ <div class="soft">
+<br>
+ <div class="modelo">
+    <h2>Back-End</h2>
+ </div>
+   
+   
+ <div id="carouselExampleControls" class="carousel slide">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="{{ asset('images/Wallpaper – Back-End I 1.png') }}" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="{{ asset('images/Wallpaper – Back-End I 1.png') }}" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="{{ asset('images/Wallpaper – Back-End II 1.png') }}" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+ 
+<br>
+<div class="detalhes">
+<a class="detalhes-botao" href="{{ asset('../desc') }}"><button class="detalhes-btn">Detalhe</button></a>
+</div>
+ 
+</div>
+ 
+ 
+ <br><br>
+<br><br>
+ 
+ 
+   
+ @include('components/include.footer')
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,14 @@
-@include("components/include.header")
+
+@include('components/include.header')
  
+<main class="aurora-main"> 
+    
 <div class="container-home">
     <div class="fundo-home">
     <h1 class="titulo-home">Study and Learn for Free With Our Courses</h1>
     <a href="{{ asset('../courses') }}" class="btn-home"><button class="botao" ID="btn-home">Enroll</button></a>
     </div>
- <img class="img-home"src="{{asset('images/React Native Code Image.png')}}">
+ <img class="img-home"src="{{ asset('images/React Native Code Image.png') }}">
 </div>
  
  
@@ -16,7 +19,7 @@
             <p class="titulo-cursos">Database</p>
         </div>
         <div class="caixa-img">
-         <img class="img-cursos" src="{{ asset('images/Wallpaper – Back-End I 1.png') }}">
+         <img class="img-cursos" src="{{ asset('images/Wallpaper – Back-End I.png') }}">
         </div>
  
         <div>
@@ -24,12 +27,20 @@
             <p class="titulo-cursos">Front-End</p>
         </div>
         <div class="caixa-img">
-         <img class="img-cursos" src="{{ asset('images/Wallpape – Front-End-1.png') }}">
+         <img class="img-cursos" src="{{ asset('images/Wallpaper – Front-End.png') }}">
         </div>
     </div>
 </div>
 </div>
 
+</main>
 
-
-@include("components/include.footer")
+ 
+ 
+ @include('components/include.footer')
+   
+ 
+ 
+   
+ 
+ 
